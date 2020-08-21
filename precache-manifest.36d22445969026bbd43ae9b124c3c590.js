8b5fbef6c8585a1848f16397f9f9f427171c5f30
Bbaale15 <bbaale.emmy@yahoo.com>
2020-08-21 16:43:15 +0000

    Updates

---
+++ precache-manifest.36d22445969026bbd43ae9b124c3c590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43a43d57330d4ea8ec342f00d5ec182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "534d84f5165219944b0e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9971e30520bc0068b896",
+    "url": "/static/js/2.a8576eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8576eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534d84f5165219944b0e",
+    "url": "/static/js/main.2b43f8a2.chunk.js"
+  },
+  {
+    "revision": "2d03142a41bbf4d3023e",
+    "url": "/static/js/runtime-main.e4e8fe5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
